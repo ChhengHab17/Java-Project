f710776f1a6232e8211c3791190e7a09237d0b81
Chhenghab <chhenghab10@gmail.com>
2025-03-11 09:55:58 +0700

    pulled rat_branch

--- src/Expense/Expense.java
+++ src/Expense/Expense.java
@@ -8,6 +8,7 @@ public class Expense {
     private int user_id;
     private int expense_id;
     private String currency;
+    private static final double EXCHANGE_RATE = 4100.0; 
 
     public Expense(String category, double amount, String date, String currency) {
         this.category = category;
@@ -16,12 +17,33 @@ public class Expense {
         this.currency = currency; 
     }
 
-    public double getAmount() {
+    public double getAmountInUSD() {
         return amount;
     }
 
+    public double getAmountInKHR() {
+        return amount * EXCHANGE_RATE;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
     public String getDetails() {
-        return "Category: " + category + ", Amount: " + amount + " " + currency + ", Date: " + date;
+        return "Category: " + category + ", Amount: $" + amount + 
+               " (KHR " + getAmountInKHR() + "), Date: " + date;
     }
 }
 
